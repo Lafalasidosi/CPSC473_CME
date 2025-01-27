f1859fcccd4d463f2ccda7d4922730e9bcb2e1da
GoldenButterMan <128435596+GoldenButterMan@users.noreply.github.com>
2025-01-27 10:24:59 -0800

    Added in exception handling and ability to add elements from txt file to a 2d array

--- apriori.py
+++ apriori.py
@@ -1,7 +1,15 @@
 import sys
+import re
 #File and min_support will be whatever is taken in as command line input
-file = sys.argv[1]
+try:
+    file = open(sys.argv[1], 'r')
+except OSError:
+    print("File could not be read.")
 min_support = sys.argv[2]
-
-def apriori(dataset, support):
-    
+#Transactions will be a 2d array that will contain all the transaction data from the file
+transactions = []
+#For loop to add each line of the file to the Transactions 2d-array
+for line in file:
+    transactions.append(re.split(r'\t| +', line))
+print(transactions[1][3])
+# def apriori(dataset, support):
