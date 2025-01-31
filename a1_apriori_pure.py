f3cf4f5ee02a345bdf770188dbfd6c1b841b3bb4
Lafalasidosi <lafalasidosi@pm.me>
2025-01-31 09:04:16 -0800

    find frequent 1-itemsets

--- a1_apriori_pure.py
+++ a1_apriori_pure.py
@@ -3,36 +3,30 @@ import sys
 
 
 #min_sup = sys.argv[2] / 100.0 * len(D)
+# e.g. len(D) = 88162 => argv[2] = 50 => minimum support = 44081
 
 
 
 
 
-def find_frequent_1_itemsets(filename):
-    # first off, create a DataFrame to hold item information
-    # this DataFrame will have two columns: id, count
-    # starts off as empty
-    # for every row of the file AFTER THE FIRST,
-    # read the row into a string except the first two elements; those aren't 'items'
-    # for every element in the resulting row, 
-    # convert it into an integer
-    # if it doesn't already exist in a DataFrame, add it with a count of 1.
-    
-    # TODO: comment this assignment out or remove entirely once method implemented in full
-    #filename = "retail-head.txt"
-    
-    counts = {}
-    data_file = open(filename)
-    while (data_file.readline()):   # skips first line
-        items = peekline(data_file).split()[2:]    # itemset portion of transaction
-        for entry in items:
-                entry = int(entry)
-                count = counts.get(entry)
-                if (count == None):
-                        counts.update([(entry, 1)])
-                else:
-                        counts.update([(entry, int(count) + 1)])
-    print(counts)
+def find_frequent_1_itemsets(filename, min_sup):    
+    counts = {}                     
+    data_file = open(filename)      
+    while (data_file.readline()):                   # skips first line containing number of rows of data
+        items = peekline(data_file).split()[2:]     # itemset portion of transaction = all columns except first two
+        for entry in items:                         # for every entry in a line, 
+                entry = int(entry)                  # read it as an integer,
+                count = counts.get(entry)           # check if it's already in `counts`:
+                if (count == None):                 # if it's not, it's the first time we've seen it
+                        counts.update([(entry, 1)]) # so add it with a count of 1
+                else:                               # otherwise, increment the value that's already there
+                        counts.update([(entry, int(count) + 1)])    
+                        
+    has_min_sup = []
+    for x in counts:
+        if counts.get(x) > min_sup:
+            has_min_sup.append((x, counts.get(x)))
+    return has_min_sup
                 
             
         
@@ -45,5 +39,5 @@ def peekline(f):
     f.seek(pos)             # set reader to where it was before the call to readline()
     return result           # return result of earlier read
 
-find_frequent_1_itemsets("a1-resources/retail-head.txt")
+find_frequent_1_itemsets("a1-resources/retail.txt", 44081)
 
