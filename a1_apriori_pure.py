a6bc696303e23d1e8c3b974ea6a3364d409f9941
Lafalasidosi <lafalasidosi@pm.me>
2025-01-31 09:16:31 -0800

    make get 1-itemset counts own function

--- a1_apriori_pure.py
+++ a1_apriori_pure.py
@@ -10,19 +10,22 @@ def main():
     L_sets.append(L1)
 
 
-def find_frequent_1_itemsets(filename, min_sup):    
-    counts = {}                     
+def get_counts(filename):
+    result = {}                     
     data_file = open(filename)      
     while (data_file.readline()):                   # skips first line containing number of rows of data
         items = peekline(data_file).split()[2:]     # itemset portion of transaction = all columns except first two
         for entry in items:                         # for every entry in a line, 
                 entry = int(entry)                  # read it as an integer,
-                count = counts.get(entry)           # check if it's already in `counts`:
+                count = result.get(entry)           # check if it's already in `counts`:
                 if (count == None):                 # if it's not, it's the first time we've seen it
-                        counts.update([(entry, 1)]) # so add it with a count of 1
+                        result.update([(entry, 1)]) # so add it with a count of 1
                 else:                               # otherwise, increment the value that's already there
-                        counts.update([(entry, int(count) + 1)])    
-                        
+                        result.update([(entry, int(count) + 1)])
+
+
+def find_frequent_1_itemsets(filename, min_sup):    
+    counts = get_counts(filename)                 
     has_min_sup = []
     for x in counts:
         if counts.get(x) > min_sup:
