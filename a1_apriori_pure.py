be803634c6979aeabed67e60aed44bcd7055dd8a
EchoDreams <lunalee0147@gmail.com>
2025-02-04 10:16:25 -0800

    changed find_frequent_n_itemsets, itemset_current.append(L_sets...) -> (L1...). Added code to create output file and write results to the file

--- a1_apriori_pure.py
+++ a1_apriori_pure.py
@@ -4,31 +4,50 @@ def main():
     #min_sup = sys.argv[2] / 100.0 * len(D)
     # e.g. len(D) = 88162 => argv[2] = 50 => minimum support = 44081
 
-    filename = "a1-resources/1k5L.txt"
-    min_sup = 10
+    filename = "a1-resources/data2_TEST.txt"
+    min_sup = 2
 
     L1 = find_frequent_1_itemsets(filename, min_sup)
-    print("L1: ", L1)
 
     L_sets = []
-    for x in range(len(L1)):
-        L_sets.append(L1[x])
+    for entry in L1:
+        L_sets.append(entry)
 
     # n-itemsets where n >= 2
     run = True
     n_max = 2
     while run:
-        print("While-loop iteration: ", n_max)
         itemset_current = []
         prev_len = len(L_sets)
-        find_frequent_n_itemsets(filename, min_sup, L1, L_sets, n_max, itemset_current, 0)
+        find_frequent_n_itemsets(filename, min_sup, L1, L_sets, n_max, itemset_current, -1)
         if prev_len == len(L_sets):
             run = False
             # when no itemsets are added in an iteration of the while-loop, it exits because there are no larger
             # frequent itemsets than the ones the program has found
         n_max += 1
 
-    print("final: ", L_sets)
+
+    # create output file
+    output_str = filename.replace("a1-resources/", "a1-resources/MiningResult_")
+    print(output_str)
+    output_file = open(output_str, "w")
+
+    # write results to file
+    output_file.write("|FPs| = " + str(len(L_sets)) + "\n")
+    for x in range(len(L_sets)):
+        # 1-itemsets
+        if x < len(L1):
+            output_file.write(str(L_sets[x][0]) + " : " + str(L_sets[x][1]) + "\n")
+        # n-itemsets
+        else:
+            itemset_str = ""
+            for element in L_sets[x][0]:
+                itemset_str += str(element) + ", "
+            output_file.write(itemset_str[:-2] + " : " + str(L_sets[x][1]) + "\n")
+
+
+
+    print("End of program.")
 
 
 def find_frequent_1_itemsets(filename, min_sup):    
@@ -38,6 +57,31 @@ def find_frequent_1_itemsets(filename, min_sup):
         if counts.get(x) > min_sup:
             has_min_sup.append((x, counts.get(x)))
     return has_min_sup
+
+
+def get_counts(filename):
+    result = {}
+    data_file = open(filename)
+    while data_file.readline():  # skips first line containing number of rows of data
+        items = peekline(data_file).split()[2:]  # itemset portion of transaction = all columns except first two
+        for entry in items:  # for every entry in a line,
+            entry = int(entry)  # read it as an integer,
+            count = result.get(entry)  # check if it's already in `counts`:
+            if count is None:  # if it's not, it's the first time we've seen it
+                result.update([(entry, 1)])  # so add it with a count of 1
+            else:  # otherwise, increment the value that's already there
+                result.update([(entry, int(count) + 1)])
+    data_file.close()
+    return result
+
+
+# Returns the current line of a given file
+def peekline(f):
+    pos = f.tell()  # set  'pos' to current reader location
+    result = f.readline()  # read whole line--reader advances to next line
+    f.seek(pos)  # set reader to where it was before the call to readline()
+    return result  # return result of earlier read
+
 
 # Parameters of find_frequent_n_itemsets:
 # filename: name of file
@@ -94,34 +138,10 @@ def find_frequent_n_itemsets(filename, min_sup, L1, L_sets, n_max, itemset_curre
     # this is the only one I could get to work.
     else:
         for x in range(len(L1)):
-            itemset_current.append(L_sets[x][0])
+            itemset_current.append(L1[x][0])
             if x > last_x:
                 find_frequent_n_itemsets(filename, min_sup, L1, L_sets, n_max, itemset_current, x)
             itemset_current.pop()
-
-
-def get_counts(filename):
-    result = {}
-    data_file = open(filename)
-    while data_file.readline():                   # skips first line containing number of rows of data
-        items = peekline(data_file).split()[2:]     # itemset portion of transaction = all columns except first two
-        for entry in items:                         # for every entry in a line,
-                entry = int(entry)                  # read it as an integer,
-                count = result.get(entry)           # check if it's already in `counts`:
-                if count is None:                 # if it's not, it's the first time we've seen it
-                        result.update([(entry, 1)]) # so add it with a count of 1
-                else:                               # otherwise, increment the value that's already there
-                        result.update([(entry, int(count) + 1)])
-    data_file.close()
-    return result
-                
-            
-# Returns the current line of a given file
-def peekline(f):
-    pos = f.tell()          # set  'pos' to current reader location
-    result = f.readline()   # read whole line--reader advances to next line
-    f.seek(pos)             # set reader to where it was before the call to readline()
-    return result           # return result of earlier read
 
 
 if __name__ == '__main__':
