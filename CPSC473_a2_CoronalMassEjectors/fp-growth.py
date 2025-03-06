364c641b87e25168131a4e166de19affc9b57bf7
GoldenButterMan <128435596+GoldenButterMan@users.noreply.github.com>
2025-03-06 15:25:23 -0800

    Initial work on fp-growth algorithm

---
+++ CPSC473_a2_CoronalMassEjectors/fp-growth.py
@@ -0,0 +1,42 @@
+from math import ceil
+import sys
+from itertools import combinations
+import linecache
+from functools import reduce
+import time
+
+def main():
+    D_name = sys.argv[1]
+    if D_name[0] == '.':
+        D_name = D_name[2:]     # no file names starting with './'
+    D = open(D_name)
+    transaction_size = int(linecache.getline(D_name, 1))
+    min_sup = ceil((int(sys.argv[2]) / 100) * transaction_size)
+    print('minsup = ' + sys.argv[2] + '% = ' + str(min_sup))
+
+def produce_output(L, D_name):
+    inputfilewithoutextension = D_name[:-4]
+    output_str = 'MiningResult_{}.txt'.format(inputfilewithoutextension)
+    output_file = open(output_str, 'w')
+    count = 0
+    print()
+    for i in L:
+        for j in i:
+            count += 1
+    output_file.write("|FPs| = " + str(count) + "\n")
+    
+    for n_itemsets in L:
+        for itemset, count in n_itemsets.items():
+            output_file.write(str(itemset) + " : " + str(count) + "\n")
+
+def peekline(f):
+    """Return the current line of an open file 
+    without moving the reader pointer.
+    """
+    pos = f.tell()
+    result = f.readline()
+    f.seek(pos)
+    return result
+
+if __name__ == '__main__':
+    main()
